d3c5dffc4ee5d6cdf50100e695f859bbde78c516
Maxim Prokopchuk <maxim.prokopchuk@invitae.com>
2022-02-24 02:26:19 +0700

    Finished Gatsby Tutorial Part 2

---
+++ src/component/layout.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+import {PropsWithChildren} from 'react'
+import { container, navLinkItem, navLinks, navLinkText, heading } from './layout.module.css'
+
+interface LayoutProps {
+  pageTitle: string
+}
+const Layout = ({ pageTitle, children }: PropsWithChildren<LayoutProps>) => {
+  return (
+    <div className={container}>
+      <title>{pageTitle}</title>
+      <nav>
+        <ul className={navLinks}>
+          <li className={navLinkItem}>
+            <Link to="/" className={navLinkText}>
+              Home
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link to="/about" className={navLinkText}>
+              About
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <h1 className={heading}>{pageTitle}</h1>
+        {children}
+      </main>
+    </div>
+  )
+}
+
+export default Layout
